a0981b17e39f58d3282cc34da8b1e9f351ba7ecb
MeirelesJv <meirelesjv@hotmail.com>
2024-02-22 10:02:47 -0300

    Primeiro Commit

---
+++ Iniciar.bat
@@ -0,0 +1,4 @@
+@echo off
+cd C:\Users\meire\OneDrive\Documentos\Node\CoinMaster\CoinMaster V1
+nodemon index.js
+pause > nul
---
+++ index.js
@@ -0,0 +1,15 @@
+const express = require("express");
+const app = express();
+const bodyParser = require("body-parser");
+
+//BodyParser
+app.use(bodyParser.urlencoded({extended: false}));
+app.use(bodyParser.json());
+
+app.set('view engine','ejs');
+
+app.use(express.static('public'));
+
+app.listen(8080,() => {
+    console.log("Servidor Iniciado")
+});
